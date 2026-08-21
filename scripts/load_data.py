ec5604a678bd1cea5aa7b2df549d919a6af2082a
Joyprakash Hela <jayhela3@gmail.com>
2026-08-21 23:01:23 +0530

    Inserted the dtudents data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -9,3 +9,16 @@ connection_string = (
 
 engine = create_engine(connection_string)
 print("Connected to SQL Server successfully")
+
+students_df = pd.read_csv("data/students.csv")
+print("CSV loaded")
+print(students_df.head())
+
+students_df.to_sql(
+    "students",
+    con = engine,
+    if_exists = "append",
+    index = False
+)
+
+print("Students data inserted successfully")
